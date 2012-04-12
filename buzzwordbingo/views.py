fe19fa7ac7f98525980e5b074bb17015531b2b58
Sean Fisk <sean@seanfisk.com>
2012-04-11 20:22:34 -0400

    Add a description to the main view using Markdown.

--- buzzwordbingo/views.py
+++ buzzwordbingo/views.py
@@ -2,6 +2,14 @@ from django.core.urlresolvers import reverse
 from djangorestframework.views import View
 
 class BuzzwordBingoView(View):
+    """The buzzword bingo REST API provides an interface to a collection of
+    boards, which contain the buzzwords on the board and a list of win
+    conditions, which are Python code which determines if a given board is a
+    winning board.
+
+    For more about the game of buzzword bingo, please see the [Buzzword Bingo
+    article on Wikipedia](http://en.wikipedia.org/wiki/Buzzword_bingo).
+    """
     def get(self, request):
         return [{'name': 'Buzzwords', 'url': reverse('buzzword-root')},
                 {'name': 'Win Conditions', 'url': reverse('win-condition-root')},
